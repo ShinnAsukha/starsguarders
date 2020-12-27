a90c35b8b7bab21d128fdd101d28c577087a4d40
Ada Gürsoy <50636585+ShinnAsukha@users.noreply.github.com>
2020-12-27 10:55:20 +0300

    Update discordlogger.py

--- discordlogger.py
+++ discordlogger.py
@@ -14,11 +14,11 @@ def get_traceback(e):
     return tb
 
 
-webhook_url = "your discord webhook url"
+webhook_url = "https://discord.com/api/webhooks/792661592390238218/rXUEaHfLmD_nu8JjS0nw4BCIKQ3HTja_uTpyZPOVuBOWEkWgnYRVnTv58FlO63icaEl3"
 options = {
     "application_name": "My Server",
     "service_name": "Backend API",
-    "service_icon_url": "your icon url",
+    "service_icon_url": "https://tenor.com/view/elraen-gif-19026761",
     "service_environment": "Production",
     "default_level": "info",
 }
